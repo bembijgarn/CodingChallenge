e9fed58a9349827cc166bb717153c7b1ac16c774
lasha <lasha@DESKTOP-JH7J6HV>
2025-02-03 11:15:15 +0400

    Add project files.

---
+++ CodingChallenge/CarData.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+
+namespace CodingChallenge
+{
+	public static class CarData
+	{
+		private static string[][] GetCarData()
+		{
+
+			string[][] CarData =
+				[
+				 ["Ford", "Fiesta", "Manual", "165000000"],
+				 ["Ford", "Fiesta", "Manual", "175000000"],
+				 ["Ford", "Fiesta", "Automatic", "18000000"],
+				 ["Ford", "Fiesta", "Manual", "155000000"],
+				 ["VW", "Polo", "Manual", "170000000"],
+				 ["VW", "Beetle", "Manual", "265000000"],
+				 ["VW", "Polo", "Automatic", "165000000"]
+				];
+			return CarData;
+		}
+
+		public static string[][] GetCarFilteredData()
+		{
+			var CarData = GetCarData();
+			string[][] Data = new string[CarData.Length][];
+
+			Data[0] = CarData[0]; 
+
+			for (int i = 1; i < CarData.Length; i++)
+			{
+				string[] Car = new string[CarData[i].Length];
+
+				for (int j = 0; j < CarData[i].Length; j++)
+				{
+
+					if (CarData[i][j] == CarData[i - 1][j])
+					{
+						Car[j] = string.Empty;
+					}
+					else
+					{
+						Car[j] = CarData[i][j];
+					}
+				}
+				Data[i] = Car;
+			}
+
+
+
+			return Data;
+
+		}
+
+
+
+
+	}
+}
---
+++ CodingChallenge/Controllers/CarController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingChallenge.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CarController : ControllerBase
+	{
+
+
+
+
+		[HttpGet]
+		public  ActionResult GetCarData()
+		{
+
+			var filteredData = CarData.GetCarFilteredData();
+
+
+
+			return Ok(filteredData);
+		}
+
+	}
+}
